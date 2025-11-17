aed6590453ad2057566d05827385d26b7e50e33f
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2025-11-17 12:47:23 +0100

    use regular fpm if present + expose gem tool to fpm

--- fpm.sh
+++ fpm.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+if type fpm >/dev/null 2>&1;then
+    fpm "$@"
+    exit
+fi
 dir=$(dirname "$(realpath "$0")")
 if [ ! -d "$dir/gems/" ] || [ ! -d "$dir/jruby/" ];then
     "$dir/setup.sh"
 fi
 export GEM_HOME=$dir/gems/
-export GEM_PATH=$dir/gems/
-export PATH=$PATH:$GEM_HOME/bin
+export PATH=$PATH:${GEM_HOME}bin:$dir/jruby/bin
 "$dir/jruby/bin/jruby" -S fpm "$@"
